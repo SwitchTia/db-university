69858fa412a637af74328842696f67c09099f1ec
SwitchTia <oblasser.tatiana@gmail.com>
2026-01-14 23:33:06 +0100

    Feat: SELECT 7

--- queries.sql
+++ queries.sql
@@ -31,7 +31,11 @@
 
 6. Selezionare tutti i corsi di laurea magistrale (38)
 
+
 7. Da quanti dipartimenti è composta l'università? (12)
+
+    SELECT *
+    FROM departments
 
 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
