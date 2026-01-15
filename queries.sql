49418881164284d67ad954ba9e436801dd41a453
SwitchTia <oblasser.tatiana@gmail.com>
2026-01-15 16:41:23 +0100

    Feat: Query con JOIN (wip)

--- queries.sql
+++ queries.sql
@@ -86,3 +86,39 @@
     GROUP BY exam_id
 
 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+    SELECT department_id, COUNT(*)
+    FROM degrees
+    GROUP BY department_id
+
+
+
+
+//Query con JOIN
+
+1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+
+2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
+
+3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
+relativo dipartimento, in ordine alfabetico per cognome e nome
+
+5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+
+7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
+superare ciascuno dei suoi esami
+
+8. Selezionare tutti gli appelli d'esame del Corso di Laurea Magistrale in Fisica del primo anno
+    
+    SELECT *
+    FROM exams
+    INNER JOIN courses
+    ON courses.id = exams.course_id
+    INNER JOIN degrees
+    ON courses.degree_id = degrees.id
+    WHERE degrees.name = "Corso di Laurea Magistrale in Fisica"
+    AND courses.year = 1
