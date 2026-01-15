eb2082cf4afe64d49a183dd3f4c07a22785a1c2e
SwitchTia <oblasser.tatiana@gmail.com>
2026-01-15 10:25:41 +0100

    Feat: GROUP

--- queries.sql
+++ queries.sql
@@ -68,7 +68,16 @@
 
 1. Contare quanti iscritti ci sono stati ogni anno
 
+    SELECT YEAR(enrolment_date), COUNT(id)
+    FROM students
+    GROUP BY YEAR(enrolment_date)
+
 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+  
+    SELECT office_address, COUNT(id)
+    FROM teachers
+    WHERE phone IS NULL
+    GROUP BY office_address;
 
 3. Calcolare la media dei voti di ogni appello d'esame
 
