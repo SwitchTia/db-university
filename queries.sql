3108cfaaf8e61c0de2ad46c99b2067649b61fc2f
SwitchTia <oblasser.tatiana@gmail.com>
2026-01-15 17:10:59 +0100

    Feat: JOIN 1

--- queries.sql
+++ queries.sql
@@ -98,6 +98,12 @@
 
 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
 
+    SELECT *
+    FROM students
+    INNER JOIN degrees
+    ON degrees.id = students.degree_id
+    WHERE degrees.name = "Corso di Laurea in Economia"
+
 2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
 
 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
