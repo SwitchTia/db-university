e9e7d4f2dbb4a778a5b193d8a29b741790407938
SwitchTia <oblasser.tatiana@gmail.com>
2026-01-15 23:22:44 +0100

    Feat: JOIN 2

--- queries.sql
+++ queries.sql
@@ -105,6 +105,12 @@
 
 2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
 
+    SELECT *
+    FROM degrees
+    INNER JOIN departments
+    ON department_id = degrees.department_id
+    WHERE degrees.level = "magistrale"
+
 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
