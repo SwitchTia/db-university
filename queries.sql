2beafaba439b41dafe15936e67e4248772d795ab
SwitchTia <oblasser.tatiana@gmail.com>
2026-01-15 23:59:02 +0100

    Feat: JOIN 3

--- queries.sql
+++ queries.sql
@@ -112,6 +112,14 @@
     WHERE degrees.level = "magistrale"
 
 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+    
+    SELECT *
+    FROM courses
+    INNER JOIN courses
+    ON courses.id = course_teacher.course_id
+    INNER JOIN course_teacher
+    ON teachers.id = course_teacher.teacher_id
+    WHERE teachers.id = 44
 
 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 relativo dipartimento, in ordine alfabetico per cognome e nome
