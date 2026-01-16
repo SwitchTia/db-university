a1edd1876a69ea734d7b09ccb0c05f247f7cc86e
SwitchTia <oblasser.tatiana@gmail.com>
2026-01-16 09:57:18 +0100

    Fix: debug

--- queries.sql
+++ queries.sql
@@ -104,28 +104,41 @@
     WHERE degrees.name = "Corso di Laurea in Economia"
 
 2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
+    
     SELECT *
     FROM degrees
     INNER JOIN departments
     ON department_id = degrees.department_id
     WHERE degrees.level = "magistrale"
+    AND departments.name = "Dipartimento di Neuroscienze"
 
 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
     
-    SELECT *
-    FROM courses
-    INNER JOIN courses
-    ON courses.id = course_teacher.course_id
+    SELECT courses, id, name, teachers.id
+    FROM teachers
     INNER JOIN course_teacher
     ON teachers.id = course_teacher.teacher_id
+    INNER JOIN courses
+    ON course_teacher.course.id = courses.id
     WHERE teachers.id = 44
 
 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 relativo dipartimento, in ordine alfabetico per cognome e nome
+    SELECT *
+    FROM students
+    INNER JOIN degrees
+    ON students.degree_id = degrees.id
+    INNER JOIN departments
+    ON degrees.department_id = department.id
+    ORDER BY students.surname, students.name
+
 
 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+
+ 
+
 
 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
 superare ciascuno dei suoi esami
